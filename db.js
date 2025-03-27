84cece5918ae9f393fc654c20657807fe82396f9
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2025-03-27 13:02:24 +0000

    modify connection string to connect to remote postgres database

--- db.js
+++ db.js
@@ -4,11 +4,15 @@ import 'dotenv/config'
 const { Pool } = pg
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Render Postgres SSL
+  },
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
 
 export default pool
