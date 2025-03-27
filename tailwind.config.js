4e90fce7bf92dd31aa4b85969589470ebcabd591
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2025-03-27 16:34:35 +0000

    futher futher old iPhone tweaking

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./client/**/*.{html,js}",
+      // Add other paths to your files that use Tailwind CSS
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
